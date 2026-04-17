f67c3c3977ebd01e8bebe8a11867898ed3ca28df
jakob63 <jakobzim20002@gmail.com>
2026-04-17 12:48:56 +0200

    startup fix

--- modules/chess-api/src/main/scala/de/eljachess/chess/api/startup/LocalUIStartup.scala
+++ modules/chess-api/src/main/scala/de/eljachess/chess/api/startup/LocalUIStartup.scala
@@ -1,8 +1,10 @@
 package de.eljachess.chess.api.startup
 
+import de.eljachess.chess.api.dto.CreateGameRequest
 import de.eljachess.chess.api.service.GameService
-import de.eljachess.chess.gui.ChessApp
+import de.eljachess.chess.gui.{ChessApp, ChessGUI}
 import de.eljachess.chess.tui.TUI
+import de.eljachess.bot.{BotOpponent, GameSetupScene, HumanOpponent}
 import io.quarkus.runtime.StartupEvent
 import jakarta.enterprise.context.ApplicationScoped
 import jakarta.enterprise.event.Observes
@@ -17,30 +19,52 @@ class LocalUIStartup:
   var service: GameService = uninitialized
 
   def onStart(@Observes event: StartupEvent): Unit =
-    val gameId = service.createGame()
-    println(s"[Chess] Local game started: $gameId")
-    service.getManager(gameId) match
-      case Left(err) => System.err.println(s"[LocalUIStartup] $err")
-      case Right(manager) =>
-        // TUI in eigenem Thread (blockiert stdin)
-        val tuiThread = Thread(
-          () =>
-            try TUI(manager).start()
-            catch case _: Exception => () // kein Terminal vorhanden
-          ,
-          "tui-thread"
-        )
-        tuiThread.setDaemon(true)
-        tuiThread.start()
+    // Show setup dialog before the board; game is created after user confirms
+    ChessApp.setupHook = stage => {
+      val setupStage = new javafx.stage.Stage()
+      setupStage.setTitle("New Chess Game")
 
-        // GUI (JavaFX) in eigenem Thread
-        val guiThread = Thread(
-          () =>
+      val setupScene = new GameSetupScene(setup => {
+        val colorStr = if setup.playerColor == de.eljachess.chess.model.Color.Black then "black" else "white"
+        val (opponentStr, botElo) = setup.opponent match
+          case HumanOpponent    => ("human", None)
+          case BotOpponent(elo) => ("bot", Some(elo.elo))
+
+        val request = CreateGameRequest(
+          playerName  = Some(setup.playerName),
+          playerColor = Some(colorStr),
+          opponent    = Some(opponentStr),
+          botElo      = botElo
+        )
+
+        val gameId = service.createGame(request)
+        println(s"[Chess] Local game started: $gameId")
+
+        service.getManager(gameId) match
+          case Left(err) => System.err.println(s"[LocalUIStartup] $err")
+          case Right(manager) =>
+            val tuiThread = Thread(
+              () => try TUI(manager).start() catch case _: Exception => (),
+              "tui-thread"
+            )
+            tuiThread.setDaemon(true)
+            tuiThread.start()
+
             ChessApp.manager       = manager
             ChessApp.lookupManager = service.getManager
-            Application.launch(classOf[ChessApp])
-          ,
-          "gui-thread"
-        )
-        guiThread.setDaemon(true)
-        guiThread.start()
+            ChessGUI(manager, stage, service.getManager).show()
+            setupStage.close()
+      })
+
+      setupStage.setScene(setupScene)
+      setupStage.setOnCloseRequest(_ => System.exit(0))
+      setupStage.show()
+    }
+
+    // GUI (JavaFX) in eigenem Thread
+    val guiThread = Thread(
+      () => Application.launch(classOf[ChessApp]),
+      "gui-thread"
+    )
+    guiThread.setDaemon(true)
+    guiThread.start()
